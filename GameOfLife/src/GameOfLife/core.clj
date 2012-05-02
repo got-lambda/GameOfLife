94433c9ac1674f1a8e84e755a8c54bfdaf069320
Erik Svedäng <erik.svedang@gmail.com>
2012-05-02 19:32:13 +0200

    Horizontal neighbor test works

--- GameOfLife/src/GameOfLife/core.clj
+++ GameOfLife/src/GameOfLife/core.clj
@@ -1,4 +1,14 @@
 (ns GameOfLife.core)
 
-(defn neighbours [world [x y]]
-  0)
+(defn neighbour-coordinates [[x y]]
+  (set (for [i [-1 0 1] j [-1 0 1] :when (not (= 0 i j))]
+	 [(+ x i) (+ y j)])))
+
+(defn alive? [world [x y]]
+  (world [x y]))
+
+(defn count-neighbours
+  [world [x y]]
+  (let [coords (neighbour-coordinates [x y])]
+    (count (filter world coords))))
+
--- GameOfLife/test/GameOfLife/test/core.clj
+++ GameOfLife/test/GameOfLife/test/core.clj
@@ -2,5 +2,17 @@
   (:use [GameOfLife.core])
   (:use [clojure.test]))
 
-(deftest count-neighbours
-  (is (= 0 (neighbours #{} [1 1]))))
+(deftest count-neighbours-empty
+  (is (= 0 (count-neighbours #{} [1 1]))))
+
+(deftest count-neighbours-above
+  (is (= 1 (count-neighbours #{[0 1]} [1 1]))))
+
+(deftest count-neighbours-left-and-right
+  (is (= 2 (count-neighbours #{[0 0] [0 2]} [0 1]))))
+
+(deftest count-neighbours-left
+  (is (= 1 (count-neighbours #{[0 0] [0 3]} [0 1]))))
+
+(deftest neighbour-coordinates-test
+  (is (= #{[0 0][0 1][0 2][1 0][1 2][2 0][2 1][2 2]} (neighbour-coordinates [1 1]))))
